557daf12079c7828bb60cf5fbd8016c4420908a6
yang-cheng-yu <158498806+yang-cheng-yu@users.noreply.github.com>
2025-05-01 14:19:18 -0400

    Added Customer and Manager clients

--- src/main/java/org/example/bankdatabasesimulation/ApplicationController.java
+++ src/main/java/org/example/bankdatabasesimulation/ApplicationController.java
@@ -10,6 +10,6 @@ import java.util.List;
 
 public class ApplicationController {
     public void init() {
-        
+
     }
 }
---
+++ src/main/java/org/example/bankdatabasesimulation/MainTwo.java
@@ -0,0 +1,9 @@
+package org.example.bankdatabasesimulation;
+
+import java.util.Date;
+
+public class MainTwo {
+    public static void main(String[] args) {
+        
+    }
+}

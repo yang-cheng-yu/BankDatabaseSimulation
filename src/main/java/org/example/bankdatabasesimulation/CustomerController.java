3c2897914e9ab4efa799f301b5d3f687e4948a9b
DavidSabb <158499251+DavidSabb@users.noreply.github.com>
2025-05-02 19:17:19 -0400

    chagnes

--- src/main/java/org/example/bankdatabasesimulation/CustomerController.java
+++ src/main/java/org/example/bankdatabasesimulation/CustomerController.java
@@ -2,11 +2,16 @@ package org.example.bankdatabasesimulation;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
-public class CustomerController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CustomerController implements Initializable {
 
     @FXML
     private TableView<?> customerTable;
@@ -33,9 +38,13 @@ public class CustomerController {
     private Button withdrawButton;
 
     @FXML
+    private Label accountBalanceLabel;
+
+    @FXML
     void deposit(ActionEvent event) {
         double money = Integer.parseInt(transactionAmoundTextField.getText());
         DatabaseHelper.deposit(DataSingleton.getInstance().getCurrentAccount().getAccountType(),money);
+        updateBalance();
     }
 
     @FXML
@@ -48,12 +57,25 @@ public class CustomerController {
         double money = Integer.parseInt(sendAmountTextField.getText());
         String email = sendemailTextField.getText();
         DatabaseHelper.transfer(DataSingleton.getInstance().getCurrentAccount().getAccountType(),money,email,AccountType.DEBIT);
+        updateBalance();
     }
 
     @FXML
     void withdraw(ActionEvent event) {
         double money = Integer.parseInt(transactionAmoundTextField.getText());
         DatabaseHelper.withdraw(DataSingleton.getInstance().getCurrentAccount().getAccountType(),money);
+        updateBalance();
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        updateBalance();
+    }
+
+    private void updateBalance(){
+        double money = DataSingleton.getInstance().getCurrentAccount().getBalance();
+        String temp =String.valueOf(money);
+        String temp2 = temp + "$";
+        accountBalanceLabel.setText(temp2);
+    }
 }
